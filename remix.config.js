327fe9ea11ac09a69fbb42b87a0564e4d8333276
Roger Studner <roger.studner@thebgroup.io>
2024-01-11 10:46:44 -0500

    First commit

---
+++ remix.config.js
@@ -0,0 +1,11 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+export default {
+  ignoredRouteFiles: ["**/.*"],
+  serverModuleFormat: "esm",
+  serverDependenciesToBundle: ["payload"],
+
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // publicPath: "/build/",
+  // serverBuildPath: "build/index.js",
+};
